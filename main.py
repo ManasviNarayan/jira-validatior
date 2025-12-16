2e9c947720c836cba9fcf6f28410b876ce316cff
ManasviNarayan <manasvinarayan2@gmail.com>
2025-12-17 02:09:17 +0530

    excel overwrite issue solved

--- main.py
+++ main.py
@@ -18,12 +18,14 @@ def main(input_csv: str = "jira_issues.csv",
     validate = run_pipeline(all_pipelines)
     first = True
     with pd.ExcelWriter(output_excel, engine="openpyxl", mode="w") as writer:
+        startrow = 0
         for df_chunk in fetch_jiras(chunk_size=chunk_size):
             df_chunk['error'] = df_chunk.apply(lambda row: validate(row), axis=1)
             df_chunk['error'] = df_chunk['error'].apply(lambda errs: '; '.join(errs) if errs else '')
             df_chunk = clean_illegal_chars(df_chunk)
-            df_chunk.to_excel(writer, index=False, header=first)
-            first = False
+
+            df_chunk.to_excel(writer, index=False, header=(startrow==0), startrow=startrow)
+            startrow += len(df_chunk)
 
 if __name__ == "__main__":
     main()
